128523c6cb5340db749a8ff6c338a36121dae66d
Erick <erick.moya@sumup.com>
2023-08-26 18:01:20 -0400

    :hammer: Add MainActivity content showing items

--- app/src/main/java/com/example/evados/MainActivity.kt
+++ app/src/main/java/com/example/evados/MainActivity.kt
@@ -4,25 +4,55 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.example.evados.data.Item
+import com.example.evados.data.ItemDao
+import com.example.evados.data.ListDB
 import com.example.evados.ui.theme.EvaDosTheme
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
+    lateinit var itemDao: ItemDao
+    val dbDispatcher: CoroutineDispatcher = Dispatchers.IO
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch(dbDispatcher) {
+            val db: ListDB = ListDB.getInstance(this@MainActivity)
+            itemDao = db.itemDao()
+        }
+
         setContent {
+            var items: List<Item> by remember { mutableStateOf(emptyList()) }
+
+            LaunchedEffect(Unit) {
+                withContext(dbDispatcher) {
+                    items = itemDao.getAll()
+                }
+            }
+
             EvaDosTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainActivityContent(items)
                 }
             }
         }
@@ -30,17 +60,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainActivityContent(items: List<Item>, modifier: Modifier = Modifier) {
+    if (items.isEmpty()) {
+        Text("No items")
+    } else {
+        LazyColumn(modifier = modifier) {
+            items(items.size) { index ->
+                Text(items[index].name)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     EvaDosTheme {
-        Greeting("Android")
+        MainActivityContent(emptyList())
     }
 }
