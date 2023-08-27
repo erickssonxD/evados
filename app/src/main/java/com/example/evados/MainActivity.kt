c0be13bb735d4b5537721505854c53a1d28bcc7d
Erick <erick.moya@sumup.com>
2023-08-26 23:24:59 -0400

    :hammer: Add floating action button

--- app/src/main/java/com/example/evados/MainActivity.kt
+++ app/src/main/java/com/example/evados/MainActivity.kt
@@ -3,9 +3,17 @@ package com.example.evados
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +22,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import com.example.evados.data.Item
@@ -26,6 +36,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     lateinit var itemDao: ItemDao
     val dbDispatcher: CoroutineDispatcher = Dispatchers.IO
@@ -58,19 +69,44 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainActivityContent(items: List<Item>, modifier: Modifier = Modifier) {
-    if (items.isEmpty()) {
-        ItemFormUI(modifier = modifier)
-    } else {
-        LazyColumn(modifier = modifier) {
-            items(items.size) { index ->
-                Text(items[index].name)
+    Scaffold(
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = { /*TODO*/ },
+                icon = {
+                    Icon(
+                        Icons.Filled.Add,
+                        contentDescription = stringResource(R.string.content_description_add_button)
+                    )
+                },
+                text = { Text(stringResource(R.string.button_add_text)) }
+            )
+        }
+    ) { contentPadding ->
+        if (items.isEmpty()) {
+            // ItemFormUI(modifier = modifier)
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(contentPadding),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = stringResource(R.string.text_no_items))
+            }
+        } else {
+            LazyColumn(modifier = modifier) {
+                items(items.size) { index ->
+                    Text(items[index].name)
+                }
             }
         }
     }
 }
 
+@ExperimentalMaterial3Api
 @Preview(showBackground = true)
 @Composable
 fun MainActivityContentFilled() {
